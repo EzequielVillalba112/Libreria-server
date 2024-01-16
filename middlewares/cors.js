1a6c2a02f22863f4c5c495c310f222ed4dd0313f
EzequielVillalba112 <148280303+EzequielVillalba112@users.noreply.github.com>
2024-01-16 20:01:05 -0300

    cors

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -3,7 +3,7 @@ import cors from 'cors'
 const ACCEPTED_ORIGINS = [
   'http://localhost:5173',
   'http://localhost:1234',
-  'https://biblioteca.com',
+  'https://ezequielvillalba112.github.io/',
 ]
 
 export const corsMiddleware = ({ acceptedOrigins = ACCEPTED_ORIGINS } = {}) => cors({
